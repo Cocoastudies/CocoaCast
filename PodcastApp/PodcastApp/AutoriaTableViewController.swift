396f8e1103970c3b63df28224d29b795ccd2e467
brunov0id <brunolabx@gmail.com>
2016-06-17 01:52:07 -0300

    adicionado deselectRowAtIndexPath na view de autoria e removido label agradecimentos e adicionado novamente no navigation item.

--- PodcastApp/PodcastApp/AutoriaTableViewController.swift
+++ PodcastApp/PodcastApp/AutoriaTableViewController.swift
@@ -11,13 +11,13 @@ import UIKit
 class AutoriaTableViewController: UITableViewController {
 
     var authorList = [
-                        ["name":"Ismael","contact":"ismaelirc@gmail.com"],
-                        ["name":"Bruno da Luz","contact":"brunolabx@gmail.com"],
-                        ["name":"Jean Carlos","contact":"jeancarlosaps@yahoo.com.br"],
-                        ["name":"Eduardo Scodino","contact":"scodino@gmail.com"],
-                        ["name":"Rafael Alencar","contact":"rafaelcsalencar@gmail.com"]
-                    ]
-    
+        ["name":"Ismael", "contact":"ismaelirc@gmail.com"],
+        ["name":"Bruno da Luz", "contact":"brunolabx@gmail.com"],
+        ["name":"Jean Carlos", "contact":"jeancarlosaps@yahoo.com.br"],
+        ["name":"Eduardo Scodino", "contact":"scodino@gmail.com"],
+        ["name":"Rafael Alencar", "contact":"rafaelcsalencar@gmail.com"]
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,34 +28,33 @@ class AutoriaTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     override func viewWillAppear(animated: Bool) {
-        
-        self.navigationController?.setNavigationBarHidden(false, animated: false);
-        
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
 
     // MARK: - Table view data source
-
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return authorList.count
     }
 
-    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
 
-        let autor = authorList[indexPath.row];
+        let autor = authorList[indexPath.row]
 
-        cell.textLabel?.text = autor["name"];
-        cell.detailTextLabel?.text = autor["contact"];
-        
+        cell.textLabel?.text = autor["name"]
+        cell.detailTextLabel?.text = autor["contact"]
+
         return cell
     }
 
